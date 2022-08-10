6e059f2d6a54bd02d2e698f45da298ed8ce1f07b
Antti Onttonen <antti@neubit.fi>
2022-08-10 09:10:14 +0300

    part_3 exercise_3.5

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
 
 let persons = [
   {
@@ -47,10 +50,16 @@ app.get('/api/persons/:id', (req, res) => {
   }
 });
 
+app.post('/api/persons/', (req, res) => {
+  const person = req.body;
+  person.id = Math.floor(Math.random() * 10000);
+  persons = persons.concat(person);
+  res.json(person);
+});
+
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id);
   persons = persons.filter((person) => person.id !== id);
-
   res.status(204).end();
 });
 
@@ -58,9 +67,3 @@ const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// 3.4: puhelinluettelon backend step4
-
-// Toteuta toiminnallisuus, jonka avulla puhelinnumerotieto on mahdollista poistaa numerotiedon yksilöivään URL:iin tehtävällä HTTP DELETE -pyynnöllä.
-
-// Testaa toiminnallisuus Postmanilla tai Visual Studio Coden REST clientillä.
